9687897224ec4c68f5c13e803ca922aa714f19de
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-07-17 07:40:36 -0700

    005

--- comps/Navbar.js
+++ comps/Navbar.js
@@ -1,12 +1,14 @@
+import Link from 'next/link'
+
 const Navbar = () => {
     return (
         <nav>
             <div className="logo">
                 <h1>ninja list</h1>
             </div>
-            <a>home</a>
-            <a>about</a>
-            <a>ninja listing</a>
+            <Link href="/"><a>home</a></Link>
+            <Link href="/about"><a>about</a></Link>
+            <Link href="/ninjas"><a>ninja listing</a></Link>
         </nav>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,15 +2,19 @@ import Head from 'next/head'
 import Footer from '../comps/Footer'
 import Navbar from '../comps/Navbar'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div>
-      <Navbar />
+      
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur maxime placeat, ut temporibus vel, rerum dicta illo molestiae ipsam qui tenetur repudiandae, quisquam cumque distinctio sunt. Autem alias doloribus pariatur quisquam ipsam.</p>
       <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia, sed! Fuga, amet in alias iste minima quae adipisci dolore quasi ducimus voluptatem voluptate odit.</p>
-      <Footer />
+      <Link href="/ninjas">
+        see ninjas listing
+      </Link>
+      
     </div>
   )
 }
