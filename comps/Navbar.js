5ff11a61ebbc522ebfc0e130ddb0393fa39c07f4
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-07-17 07:27:01 -0700

    033

---
+++ comps/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>ninja list</h1>
+            </div>
+            <a>home</a>
+            <a>about</a>
+            <a>ninja listing</a>
+        </nav>
+    )
+}
+
+export default Navbar
