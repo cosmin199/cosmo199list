07cbd441f9a68b70321a6a1f659f93f808b5775e
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-07-19 07:06:55 -0700

    010

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,7 +1,26 @@
-const Ninjas = () => {
+import styles from '../../styles/Ninjas.module.css'
+export const getStaticProps = async () => {
+
+   const res = await fetch('https://jsonplaceholder.typicode.com/users')
+   const data = await res.json()
+
+   return {
+       props: { ninjas: data }
+   }
+
+}
+
+const Ninjas = ({ ninjas }) => {
     return ( 
         <div>
             <h1>all ninjas</h1>
+            {ninjas.map(ninja => (
+                <div key={ninja.jd}>
+                    <a className={styles.single}>
+                        <h3>{ ninja.name }</h3>
+                    </a>
+                </div>
+            ))}
         </div>
      );
 }
