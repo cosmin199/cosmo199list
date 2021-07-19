b07a5b62bebbc206389423f30ca2b4c0eb0ba15b
cosmin199 <cosmin_ionescu_email@yahoo.ro>
2021-07-19 08:22:47 -0700

    013

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -4,9 +4,9 @@ export const getStaticPaths = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await res.json()
 
-    const paths = data.map(jinja => {
+    const paths = data.map(ninja => {
         return {
-            params: { id: Ninjas.id.toString() }
+            params: { id: ninja.id.toString() }
         }
     })
 
@@ -16,10 +16,24 @@ export const getStaticPaths = async () => {
     }
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+    const id = context.params.id
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+    const data = await res.json()
+
+    return {
+        props: { ninja: data}
+    }
+}
+
+const Details = ({ ninja }) => {
     return (
         <div>
-            <h1>Details Page</h1>
+            <h1>{ninja.name}</h1>
+            <p>{ninja.email}</p>
+            <p>{ninja.website}</p>
+            <p>{ninja.address.city}</p>
+            
         </div>
     )
 }
